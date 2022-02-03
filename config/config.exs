228493dc32d052a820d6f5206cece1c7cd69386a
Simon Benitez <sbenitez@tubi.tv>
2022-02-03 17:37:28 -0500

    [update] update log level to warn

--- config/config.exs
+++ config/config.exs
@@ -30,6 +30,8 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :logger, level: :warn
+
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
